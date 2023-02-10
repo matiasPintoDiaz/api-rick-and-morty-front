ee536de9f29297f12bda6b9d180da59ee8a77707
Matias Pinto <pintomatias2@gmail.com>
2023-02-09 23:25:30 -0300

    Pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import CharacterPage from './pages/CharacterPage'
+import EpisodePage from "./pages/EpisodePage"
+import LocationPage from "./pages/LocationPage"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    <Routes>
+      <Route path='/' element={App} />
+      <Route path='/CharacterPage' element={CharacterPage} />
+      <Route path='/EpisodePage' element={EpisodePage} />
+      <Route path='/LocationPage' element={LocationPage} />
+    </Routes>
+  </BrowserRouter>,
 )
---
+++ src/pages/EpisodePage.tsx
@@ -0,0 +1,7 @@
+export default function EpisodePage() {
+    return(
+        <div>
+            EPISODES
+        </div>
+    )
+}
