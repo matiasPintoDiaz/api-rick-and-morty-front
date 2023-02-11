96d08010cf093ab02cd10cdf07df9e9bc0369396
Matias Pinto <pintomatias2@gmail.com>
2023-02-11 00:00:03 -0300

    episodes accordion

--- src/pages/EpisodePage.tsx
+++ src/pages/EpisodePage.tsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import "./episodesTemp.css";
 
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 import { fetchEpisode, episodeData } from "../services/fetchingEpisode";
 import { baseEpisodeData } from "../services/basesTypes";
 
@@ -8,24 +14,56 @@ export default function EpisodePage() {
   const [episodes, setEpisodes] = useState([]);
 
   useEffect(() => {
-    fetchEpisode().then(setEpisodes)
-    console.log(episodes)
+    fetchEpisode().then(setEpisodes);
+    console.log(episodes);
   }, []);
 
   return (
     <>
-      <div className="z">
-        <ol className="episode-list">
+      <div className="list-box">
+        <ul className="episode-list">
           {episodes.map((episode) => (
             <div className="episode-box">
-              <li className="episode">
-                {episode.name}
-              </li>
+              {/* <li className="episode">{episode.name}</li> */}
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>{episode.name}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget. */}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
               {/* <p>{episode.episode}</p>
               <p>{episode.air_date}</p> */}
             </div>
           ))}
-        </ol>
+        </ul>
+      </div>
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       </div>
     </>
   );
