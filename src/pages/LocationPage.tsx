e59906679e8f4daaab01e581a51491985b6fae42
Matias Pinto <pintomatias2@gmail.com>
2023-02-17 22:25:11 -0300

    accordion for locations

--- src/pages/LocationPage.tsx
+++ src/pages/LocationPage.tsx
@@ -1,11 +1,19 @@
 import { useState, useEffect } from 'react';
-import "../css/locationPage.css"
+import "../css/locationPage.css";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 
 import { fetchLocation, locationData } from "../services/fetchingLocation"
 
 export default function LocationPage() {
   const [id, setId] = useState(1);
   const [location, setLocation] = useState<locationData>();
+  const [expanded, setExpanded] = useState<string | false>(false);
 
   useEffect(() => {
     fetchLocation(id).then(setLocation)
@@ -25,6 +33,11 @@ export default function LocationPage() {
     setLocation(locationResponse);
   };
 
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
+
   return (
     <>
       <div className='location-box'>
@@ -35,6 +48,34 @@ export default function LocationPage() {
           <button className='location-button' onClick={() => setCountOp("sub")}>Previous Location</button>
           <p className='location-id'>Ubicación número: {id}</p>
           <button className='location-button' onClick={() => setCountOp("add")}>Next Location</button>
+        </div>
+        <div className="list-box">
+          {location && (
+            <Accordion
+              className="accordion"
+              expanded={expanded === `${location.name}`}
+              onChange={handleChange(`${location.name}`)}
+              key={location.id.toString()}
+            >
+              <AccordionSummary
+                className="accordion"
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id={location.name}
+              >
+                <Typography>{location.name}</Typography>
+              </AccordionSummary>
+              <AccordionDetails className="accordion">
+                <Typography>
+                  <div>
+                    <p>Tipo: {location.type}</p>
+                    <p>Dimensión: {location.dimension}</p>
+                    {/* <p>Residentes: {location.residents}</p> */}
+                  </div>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          )}
         </div>
       </div>
     </>
