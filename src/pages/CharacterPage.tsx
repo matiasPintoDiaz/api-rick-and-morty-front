5a6c9096f523a914ce4d3d31c398ce03add054ac
Matias Pinto <pintomatias2@gmail.com>
2023-02-18 17:36:36 -0300

    base character page

--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -1,3 +1,39 @@
+import { useState, useEffect } from 'react';
+import "../css/characterpage.css";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+import { fetchCharacter, CharacterData } from "../services/fetchingCharacter";
+
 export default function CharacterPage() {
-  return <div>CHARACTERS</div>;
+  const [id, setId] = useState(1);
+  const [character, setCharacter] = useState<CharacterData>();
+
+  useEffect(() => {
+
+  });
+
+  // fetching + setId(count)
+
+  return (
+    <>
+      <div className="character-box">
+        <div>
+          <h1>Rick & Morty Characters</h1>
+        </div>
+        <div className="buttons-character-box">
+          <button className='character-button'>Previous Character</button>
+          <p className='character-id'>Personaje número: {id}</p>
+          <button className='character-button'>Next Character</button>
+        </div>
+        <div className="list-box">
+
+        </div>
+      </div>
+    </>
+  );
 }
