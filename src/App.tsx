976dce2a0253bb993df6cf0e111724ad2d9006d4
Matias Pinto <pintomatias2@gmail.com>
2023-02-18 17:47:05 -0300

    base app home

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,17 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-
 import { fetchCharacter, CharacterData } from "./services/fetchingCharacter";
 
 function App() {
-  const [count, setCount] = useState(1);
-  const [character, setCharacter] = useState<CharacterData>();
-
-  useEffect(() => {
-    fetchCharacter(count).then(setCharacter)
-  }, [count]);
-
-  const setCountOp = async (op: string) => {
-    if (op == "sub") {
-      if (count <= 1) return;
-      setCount(count - 1);
-    } else {
-      setCount(count + 1);
-    }
-
-    const character = await fetchCharacter(count);
-    console.log(character);
-    setCharacter(character);
-  };
-
   return (
     <>
       <div className="App">
         <div>
-          <h1>Rick & Morty</h1>
+          <h1>Rick & Morty API</h1>
         </div>
-        <div className="card">
-          <button className="character-buton" onClick={() => setCountOp("sub")}>Previous Character</button>
-          <p className="character-id">Personaje número: {count}</p>
-          <button className="character-buton" onClick={() => setCountOp("add")}>Next Character</button>
-        </div>
-        <div className="character-card">
-          {character && (
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="300"
-                  image={character.image}
-                  alt="character image"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {character.name}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          )}
-        </div>
+        <div className="card"></div>
+        <div className="character-card"></div>
       </div>
     </>
   );
